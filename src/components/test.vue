4757065832e84bb1f36cf869a288de2653826d8f
gabriel Michard <gabriel.michard@supelec.fr>
2021-02-10 18:05:41 +0100

    bouton

---
+++ src/components/test.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="test">
+  <button v-on:click="nickel">Add 1</button>
+  <p>Le bouton ci-dessus a été cliqué {{ counter }} fois.</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      counter: 1
+    }
+  },
+  methods: {nickel: function (event) { alert('NTM') }}
+}
+</script>
