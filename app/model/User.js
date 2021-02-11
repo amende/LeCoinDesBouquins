4b05fc8c1a8a0bfc6c1a107ee0e24827ccd52c3f
HugoMichard <hugomichard@yahoo.com>
2021-02-11 18:41:41 +0100

    fix register

--- app/model/User.js
+++ app/model/User.js
@@ -2,13 +2,13 @@ var sql = require('./db.js')
 
 var User = function (user) {
   this.email = user.email
-  this.password = user.password
+  this.pwd = user.pwd
   this.created_at = new Date()
 }
 
 User.register = function (newUser, result) {
   sql.query(
-    'INSERT INTO user set ?', newUser, function (err, res) {
+    'INSERT INTO USER set ?', newUser, function (err, res) {
       if (err) {
         console.log('error in register model: ', err)
         result(null, res.insertId)
--- src/components/inscription.vue
+++ src/components/inscription.vue
@@ -2,18 +2,18 @@
 <form>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Votre adresse mail :</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="email">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="form.email">
     <div id="emailHelp" class="form-text">Nous ne divulguerons cette adresse à personne (inch allah)</div>
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Votre mot de passe (lui non plus on le révèle pas): </label>
-    <input type="password" class="form-control" id="exampleInputPassword1" v-model="pwd">
+    <label for="exampleInputPassword1" class="form-label">Votre mot de passe (lui non plus on le révèle pas) : </label>
+    <input type="password" class="form-control" id="exampleInputPassword1" v-model="form.pwd">
   </div>
   <div class="mb-3 form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">cochez ici, ça ne sert à rien mais ça fait classe</label>
   </div>
-  <b-button variant="primary" v-on:click="submit()">Je m'inscris !</b-button>
+  <b-button variant="primary" v-on:click="submit">Je m'inscris !</b-button>
 </form>
 
 </template>
@@ -26,13 +26,15 @@ export default {
   name: 'inscription',
   data () {
     return {
-      email: '',
-      pwd: ''
+      form: {
+        email: '',
+        pwd: ''
+      }
     }
   },
   methods: {
     submit () {
-      apiService.register(this.data)
+      apiService.register(this.form)
     }
   }
 }
